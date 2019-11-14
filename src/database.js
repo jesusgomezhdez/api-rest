c031535017a4beb45cde9540ede019293753066e
Jesus Gomez <gomezhdez.jesus@gmail.com>
2019-11-14 12:15:46 -0600

    quité el topology de la conexión

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import MongoClient from 'mongodb';
 
 export async function connect() {
     try {
-        const client = await MongoClient.connect('mongodb+srv://jesusgomez:gomez12@cluster0-qc7h2.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+        const client = await MongoClient.connect('mongodb+srv://jesusgomez:gomez12@cluster0-qc7h2.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true});
         const db = client.db('test');
         console.log('DB is connected');
         return db;
