f9d4abf493fff1f4f9fd1b4a3da9d069eea67f61
Jesus Gomez <gomezhdez.jesus@gmail.com>
2019-11-14 11:44:17 -0600

    se modificó la bd

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import MongoClient from 'mongodb';
 
 export async function connect() {
     try {
-        const client = await MongoClient.connect('mongodb+srv://jesusgomez:gomez12@cluster0-qc7h2.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+        const client = await MongoClient.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
         const db = client.db('test');
         console.log('DB is connected');
         return db;
